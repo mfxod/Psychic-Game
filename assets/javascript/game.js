f4997b0d1775234b5a68efa51e9eaf3122f68640
mfxod <46651745+mfxod@users.noreply.github.com>
2019-05-29 22:16:58 -0400

    noted way to check that user input is a letter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,6 @@
+// String.fromCharCode() | check against unicode #s for a-z
+// alert("Enter a letter.")
+
 // declare variables and reset function:
 
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
